b52209aeabb25f3c6a9ffc962b4b94d404ca1fac
cmancrypto <cmancrypto@outlook.com>
2023-03-26 19:28:29 +1100

    generalised: delegationsOnOtherChains
    added: networkconfig

--- scripts/delegationsOnOtherChains.py
+++ scripts/delegationsOnOtherChains.py
@@ -3,33 +3,64 @@ import pandas as pd
 import json
 import delegatorSnapshotProcessing as helper
 import utils 
+import yaml
+from cosmpy.aerial.client import LedgerClient, NetworkConfig
 
-def main(): 
-    url="https://juno-api.kleomedes.network/cosmos/staking/v1beta1/validators/junovaloper1x8u2ypdr35802tjyjqyxan8x85fzxe6sk0qmh8/delegations?pagination.limit=50000"
-    delegation_response=helper.snapshotDelegatorsUsingAPI(url)
+def main(sourcechain,chaintoanalyse): 
+    delegation_response=getValidatorDelegationResponseFromAPI(sourcechain)
     dfDelegators=helper.convertJSONtoDataFrame(delegation_response)
-    akash_addresses=[]
-    for delegator in dfDelegators["address"]:
-        akash_address=utils.convert_address_to_address("juno",delegator,"akash")
-        akash_addresses.append(akash_address)
-    print(len(akash_addresses))
-    
-    akash_balances=[]
-
-    cfg=utils.getAkashCFG()
-    client=utils.getCosmpyClient(cfg)
-
-    for index,address in enumerate(akash_addresses):
-        print(index)
-        try:
-            s=client.query_staking_summary(address)
-            if s.total_staked>0:
-                akash_balances.append([address,s.total_staked])
-                print(akash_balances)
-        except:
-            print("exception occured")
-    df=pd.DataFrame(akash_balances)
-    df.to_csv("akash_balances_2.csv")
+    chain_addresses=getChainToAnalyseAddresses(sourcechain,chaintoanalyse,dfDelegators)
+    networkcfg= getNetworkConfig(chaintoanalyse)
+    ledgercfg=getLedgerClientConfig(networkcfg)
+    ledgerclient=utils.getCosmpyClient(ledgercfg)
+    print(f"Querying {sourcechain} delegators to Kleomedes balances on {chaintoanalyse}")
+    chainBalancesByAddress=queryDelegatedBalancesByAddressList(chain_addresses,ledgerclient)
+    df=pd.DataFrame(chainBalancesByAddress)
+    df.to_csv(f"balances{chaintoanalyse}.csv")
             
+def getValidatorDelegationResponseFromAPI(sourcechain):
+    sourcechainconfig=getNetworkConfig(sourcechain)
+    validatoraddress=sourcechainconfig["validator"]
+    api=sourcechainconfig["api"]
+    query=f"cosmos/staking/v1beta1/validators/{validatoraddress}/delegations?pagination.limit=50000"
+    url=f"{api}{query}"
+    delegation_response=helper.snapshotDelegatorsUsingAPI(url)
+    return delegation_response
 
-main()
+def getChainToAnalyseAddresses(sourcechain,chaintoanalyse,dfDelegators):
+    chain_addresses=[]
+    sourceconfig=getNetworkConfig(sourcechain)
+    chaintoanalyseconfig=getNetworkConfig(chaintoanalyse)
+    for delegator in dfDelegators["address"]:
+        chain_address=utils.convert_address_to_address(sourceconfig["addressprefix"],delegator,chaintoanalyseconfig["addressprefix"])
+        chain_addresses.append(chain_address)
+    return chain_addresses
+
+def getNetworkConfig(chain):
+    with open("networkconfig.yaml", "r") as file:   
+        networkconfig=yaml.safe_load(file)
+        chainconfig=networkconfig["networks"][chain]
+    return chainconfig
+
+def getLedgerClientConfig(chainconfig):
+    config=chainconfig["config"]
+    ledgercfg=NetworkConfig(**config)
+    print(ledgercfg)
+    return ledgercfg
+
+def queryDelegatedBalancesByAddressList(chain_addresses,ledgerclient):
+    chainBalancesByAddress=[]   
+    for index,address in enumerate(chain_addresses):
+            print(f"{index} of {len(chain_addresses)}") 
+            try:
+                s=ledgerclient.query_staking_summary(address)
+                if s.total_staked>0:
+                    chainBalancesByAddress.append([address,s.total_staked])
+            except Exception as e:
+                print("exception occured")
+                print(e)
+    return(chainBalancesByAddress) 
+
+
+main("juno","akash")
+main("juno","injective")
