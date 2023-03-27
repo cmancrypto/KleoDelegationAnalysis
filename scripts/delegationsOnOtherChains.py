726ab02c8f91c74f3bf1717b29e78b1cea656375
cmancrypto <cmancrypto@outlook.com>
2023-03-28 06:53:49 +1100

    changing methodology so only query addresses that exist on chain

--- scripts/delegationsOnOtherChains.py
+++ scripts/delegationsOnOtherChains.py
@@ -15,7 +15,8 @@ def main(sourcechain,chaintoanalyse):
     ledgercfg=getLedgerClientConfig(networkcfg)
     ledgerclient=utils.getCosmpyClient(ledgercfg)
     print(f"Querying {sourcechain} delegators to Kleomedes balances on {chaintoanalyse}")
-    chainBalancesByAddress=queryDelegatedBalancesByAddressList(chain_addresses,ledgerclient)
+    ##chainBalancesByAddress=queryDelegatedBalancesByAddressList(chain_addresses,ledgerclient)
+    chainBalancesByAddress=queryDelegatedBalancesByAddressListAPI(chain_addresses[1:300],chaintoanalyse)
     df=pd.DataFrame(chainBalancesByAddress)
     df.to_csv(f"balances{chaintoanalyse}.csv")
             
@@ -70,6 +71,87 @@ def queryDelegatedBalancesByAddressList(chain_addresses,ledgerclient):
                     print("retry failed") 
     return(chainBalancesByAddress) 
 
+def queryDelegatedBalancesByAddressListAPI(chain_addresses, chaintoanalyse):
+     chainBalancesByAddress=[]
+     chainconfig=getNetworkConfig(chaintoanalyse)
+     api=chainconfig["api"]
 
-main("juno","akash")
-main("juno","injective")
+     for index,address in enumerate(chain_addresses):
+        print(f"{index} of {len(chain_addresses)}") 
+        url=f"{api}cosmos/staking/v1beta1/delegations/{address}"
+        try:
+            delegation_response=helper.snapshotDelegatorsUsingAPI(url)
+            sum_delegator=0
+            if len(delegation_response)>0:
+                for delegations in delegation_response:
+                    delegation=float(delegations["delegation"]["shares"])
+                    sum_delegator=delegation+sum_delegator
+                chainBalancesByAddress.append(sum_delegator)
+        except Exception as e:
+            print("exception occured")
+            print(e)
+            time.sleep(15)
+            try:
+                delegation_response=helper.snapshotDelegatorsUsingAPI(url)
+                sum_delegator=0
+                if len(delegation_response)>0:
+                    for delegations in delegation_response:
+                        delegation=float(delegations["delegation"]["shares"])
+                        sum_delegator=delegation+sum_delegator
+                    chainBalancesByAddress.append(sum_delegator)
+            except Exception as e:
+                print("exception occured")
+                print(e) 
+
+     return chainBalancesByAddress
+
+def queryGetAllAccounts(chaintoanalyse):
+     chainconfig=getNetworkConfig(chaintoanalyse)
+     api=chainconfig["api"]
+     url=f"{api}cosmos/auth/v1beta1/accounts?pagination.limit=500000"
+     print(url)
+     response=requests.get(url)
+     return response.json()
+
+def compareDelegatorsWithAnalysisChain(sourcechain,chaintoAnalyse):
+    response=queryGetAllAccounts(chaintoAnalyse)["accounts"]
+    accountsOnChain=[]
+    ##so far, injective accounts are treated differently to akash - unsure if all same. BaseAcccount is for Akash, Injective is EthAccount
+    for accounts in response:
+        if accounts["@type"]=="/cosmos.auth.v1beta1.BaseAccount":
+            try:
+                accountsOnChain.append(accounts["address"])
+            except Exception as e:
+                print(accounts)
+                print(f"exception{e}")
+        if accounts["@type"]=="/injective.types.v1beta1.EthAccount":
+            try:
+                accountsOnChain.append(accounts["base_account"]["address"])
+            except Exception as e:
+                print(accounts)
+                print(f"exception{e}")
+        else: 
+            print(accounts)
+
+    dfAccountsOnChain=pd.DataFrame(accountsOnChain,columns=["address"])
+
+    dfDelegators=getDelegatorsAndConvert(sourcechain)
+
+    chain_addresses=getChainToAnalyseAddresses(sourcechain,chaintoAnalyse,dfDelegators)
+    dfchain_addresses=pd.DataFrame(chain_addresses,columns=["address"])
+
+    comparison=helper.createComparisonDelegatorDataFrame(dfAccountsOnChain,dfchain_addresses)
+
+    print(comparison)
+    return comparison
+
+def getDelegatorsAndConvert(chain):
+    delegation_response=getValidatorDelegationResponseFromAPI(chain)
+    dfDelegators=helper.convertJSONtoDataFrame(delegation_response)
+    return dfDelegators
+
+
+##main("juno","akash")
+##main("juno","injective")
+compareDelegatorsWithAnalysisChain("juno","akash")
+
